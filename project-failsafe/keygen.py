fae635091fa373df0ffb1d33197d0388bc8f1cc8
nepeat <nepeat@users.noreply.github.com>
2017-12-16 00:00:56 -0800

    project-failsafe keygen

---
+++ project-failsafe/keygen.py
@@ -0,0 +1,25 @@
+import hashlib
+from typing import List
+
+def md5(password: str):
+    return hashlib.md5(password.encode("utf8")).hexdigest()
+
+def generate_failsafe_password(passwords: List[str]):
+    result = ""
+
+    passwords = sorted(passwords)
+    for password in passwords:
+        result += md5(password)
+
+    return result
+
+if __name__ == "__main__":
+    passwords = []
+
+    with open("passwords", "r") as f:
+        data = f.read()
+        for line in data.split("\n"):
+            passwords.append(line)
+
+    combined_password = generate_failsafe_password(passwords)
+    print(f"Veracrypt password: {combined_password}")
