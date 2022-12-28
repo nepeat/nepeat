5397ec4102e88162863346f48b3ed71512f96759
erin.liman <erin.liman@bytedance.com>
2022-12-27 16:28:57 -0800

    feat: hybrid personal/work dotfiles

---
+++ dotfiles/setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -x
+
+# install homebrew if not found
+if ! command -v brew &> /dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# install brew packages
+xargs brew -v install < brew/formula
+xargs brew -v install --cask < brew/casks
+
+# link code -> vscode
+if ! command -v vscode &> /dev/null; then
+    sudo ln -svf /Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code /usr/local/bin/vscode
+fi
+
+# install rust stable + nightly
+if ! command -v rustc &> /dev/null
+then
+    rustup-init -v --no-modify-path --default-toolchain nightly
+    source "$HOME/.cargo/env"
+    rustup install stable
+fi
+
+# enable dark mode + ui tweaks
+osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode'
+defaults -currentHost write com.apple.controlcenter.plist BatteryShowPercentage -bool true
+defaults -currentHost write com.apple.menuextra.clock.plist ShowSeconds -bool true
+defaults write com.apple.dock "autohide" -bool "true"
+
+# kill after settings updates
+killall Dock
+
+if [[ -d "${INTERNAL_DOTFILES}" ]]; then
+    cd "${INTERNAL_DOTFILES}"
+    make setup
+fi
