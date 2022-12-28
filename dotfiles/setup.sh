0514273387030ebee56156ba850e95fd36b46e82
nepeat <nepeat@gmail.com>
2022-12-28 00:13:39 -0800

    feat: rvm for ruby

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -31,6 +31,14 @@ if ! command -v node &> /dev/null; then
 	nvm install --lts
 fi
 
+# install rvm + latest ruby
+gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+if ! command -v rvm &> /dev/null; then
+	curl -sSL https://get.rvm.io | bash -s stable
+	[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+	rvm install ruby
+fi
+
 # enable dark mode + ui tweaks
 osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
 defaults -currentHost write com.apple.controlcenter.plist BatteryShowPercentage -bool true
