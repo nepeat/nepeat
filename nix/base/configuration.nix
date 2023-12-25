360163f781a9aa3a2fd7a74855118b0d322fce4e
nepeat <nepeat@gmail.com>
2023-12-25 14:21:49 +0100

    feat: remove deprecated nix stuff + use cachix

--- nix/base/configuration.nix
+++ nix/base/configuration.nix
@@ -5,16 +5,21 @@
 
   # Nix configuration ------------------------------------------------------------------------------
 
-  nix.binaryCaches = [
+  nix.settings.substituters = [
     "https://cache.nixos.org/"
+    "https://nix-community.cachix.org"
   ];
-  nix.binaryCachePublicKeys = [
+
+  nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
-  nix.trustedUsers = [
+
+  nix.settings.trusted-users = [
     "@admin"
   ];
-  users.nix.configureBuildUsers = true;
+
+  nix.configureBuildUsers = true;
 
   # Enable experimental nix command and flakes
   # nix.package = pkgs.nixUnstable;
@@ -47,7 +52,7 @@
   programs.nix-index.enable = true;
 
   # Fonts
-  fonts.enableFontDir = true;
+  fonts.fontDir.enable = true;
   fonts.fonts = with pkgs; [
      recursive
      (nerdfonts.override { fonts = [
