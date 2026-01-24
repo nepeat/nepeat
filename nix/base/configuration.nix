7bd8c77f93d441297e91e6ecc5bee4852514cca9
nepeat <nepeat@gmail.com>
2026-01-23 17:34:02 -0800

    fix: touchid only darwin

--- nix/base/configuration.nix
+++ nix/base/configuration.nix
@@ -45,5 +45,5 @@
   programs.nix-index.enable = true;
 
   # Add ability to used TouchID for sudo authentication
-  security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin true;
 }
