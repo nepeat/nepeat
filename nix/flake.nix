4d18500e42c0a06c5e0a8094759d2ee4516bb597
nepeat <nepeat@gmail.com>
2026-03-21 18:39:44 -0700

    chore: disable rosetta builder on air

--- nix/flake.nix
+++ nix/flake.nix
@@ -100,18 +100,6 @@
             home-manager.useUserPackages = true;
             home-manager.users.nepeat = import ./base/home.nix;
           }
-          # An existing Linux builder is needed to initially bootstrap `nix-rosetta-builder`.
-          # If one isn't already available: comment out the `nix-rosetta-builder` module below,
-          # uncomment this `linux-builder` module, and run `darwin-rebuild switch`:
-          # { nix.linux-builder.enable = true; }
-          # Then: uncomment `nix-rosetta-builder`, remove `linux-builder`, and `darwin-rebuild switch`
-          # a second time. Subsequently, `nix-rosetta-builder` can rebuild itself.
-          nix-rosetta-builder.darwinModules.default
-          {
-            # see available options in module.nix's `options.nix-rosetta-builder`
-            nix-rosetta-builder.onDemand = true;
-            environment.etc."machine_name".text = "newPersonal";
-          }
         ];
       };
       personal = darwinSystem {
