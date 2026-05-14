53e734a7db0a3701b6fdc439f57f7b1111789eb8
nepeat <nepeat@gmail.com>
2026-05-13 17:53:31 -0700

    feat: refactor nix.settings + enable auto-optimize-store

--- nix/base/configuration.nix
+++ nix/base/configuration.nix
@@ -6,28 +6,31 @@
 
   nix.distributedBuilds = true;
 
-  nix.settings.substituters = [
-    "https://cache.nixos.org/"
-    "https://nix-community.cachix.org"
-  ];
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
 
-  nix.settings.trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  ];
+  trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
 
-  nix.settings.trusted-users = [
-    "@admin"
-  ];
+    trusted-users = [
+      "@admin"
+    ];
 
-  # Enable experimental nix command and flakes
-  # nix.package = pkgs.nixUnstable;
-  nix.extraOptions = ''
-    auto-optimise-store = false
-    experimental-features = nix-command flakes
-  '' + lib.optionalString (pkgs.stdenv.hostPlatform.system == "aarch64-darwin") ''
-    extra-platforms = x86_64-darwin aarch64-darwin
-  '';
+    auto-optimise-store = true;
+
+    # Enable experimental nix command and flakes
+    # nix.package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '' + lib.optionalString (pkgs.stdenv.hostPlatform.system == "aarch64-darwin") ''
+      extra-platforms = x86_64-darwin aarch64-darwin
+    '';
+  };
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.bash.enable = true;
