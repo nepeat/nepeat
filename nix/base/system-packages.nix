283c66b557a53f1b91a35f9f655442650e6d965c
nepeat <nepeat@gmail.com>
2026-07-11 13:22:59 -0700

    fix: remove notifier package

--- nix/base/system-packages.nix
+++ nix/base/system-packages.nix
@@ -4,7 +4,6 @@
   # `home-manager` currently has issues adding them to `~/Applications`
   # Issue: https://github.com/nix-community/home-manager/issues/1341
   environment.systemPackages = with pkgs; [
-    terminal-notifier
     file
 
     # hsm + smartcard stuff
